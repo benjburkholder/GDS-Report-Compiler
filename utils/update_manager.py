394972ee068f47ce1079ee2010dc82caf66a333f
Jake Schroeder <jschroeder@linkmedia360.com>
2020-07-27 08:55:16 -0400

    Documentation

--- utils/update_manager.py
+++ utils/update_manager.py
@@ -2,6 +2,7 @@
 Update Manager Module
 """
 import os
+import json
 import pathlib
 import requests
 import zipfile
@@ -9,12 +10,14 @@ import shutil
 
 
 class UpdateManager:
+    """
+    Class responsible for handling remote updates of static platform components
+    ========================================================================================================
+    """
 
     base_uri = 'https://api.github.com/repos/:username/:repository/releases'
 
     update_key = 'zipball_url'
-
-    release_version = None
 
     def __init__(self, auth_token: str, username: str, repository: str):
         self.headers = {
@@ -22,6 +25,7 @@ class UpdateManager:
         }
         self.username = username
         self.repository = repository
+        self.release_version = self._get_latest_release_version()
 
     current_release_idx = 0  # first item back is the latest release from github
 
@@ -29,8 +33,24 @@ class UpdateManager:
 
     current_release_archive_key = 'zipball_url'
 
+    def _get_latest_release_version(self) -> str:
+        """
+        Finds and retrieves the latest version number based on release recency
+        ====================================================================================================
+        :return:
+        """
+        releases = self._get_releases()
+        current_release = releases[self.current_release_idx]
+        return current_release[self.current_release_version_key]
+
     def download_latest(self):
-        self.clear_update_zone()
+        """
+        Finds and downloads the latest release .zip archive to the project /tmp directory
+        ====================================================================================================
+        :return:
+        """
+        if os.path.exists(self.__get_save_path()):
+            self.clear_update_zone()
         releases = self._get_releases()
         current_release = releases[self.current_release_idx]
         self.release_version = current_release[self.current_release_version_key]
@@ -46,6 +66,12 @@ class UpdateManager:
     bad_response_text = "Bad response from GitHub servers, "
 
     def _get_archive(self, archive_url: str):
+        """
+        Make a secure HTTP request to get the .zip release archive by URL
+        ====================================================================================================
+        :param archive_url:
+        :return:
+        """
         response = requests.get(
             url=archive_url,
             headers=self.headers
@@ -54,6 +80,11 @@ class UpdateManager:
         return response
 
     def _get_releases(self):
+        """
+        Make a secure HTTP request to get the platform's releases
+        ====================================================================================================
+        :return:
+        """
         # make the request and parse the response for the update_key
         url = self.base_uri.replace(':username', self.username).replace(':repository', self.repository)
         response = requests.get(
@@ -66,11 +97,21 @@ class UpdateManager:
     project_root_idx = 1  # two directories down from here
 
     def __get_project_root(self):
+        """
+        Returns the global location of the project root
+        ====================================================================================================
+        :return:
+        """
         return pathlib.Path(__file__).parents[self.project_root_idx]
 
     tmp_dir_name = 'tmp'
 
     def __get_save_path(self):
+        """
+        Returns the global location of the projects /tmp directory where updates are made and handled
+        ====================================================================================================
+        :return:
+        """
         return os.path.join(
             self.__get_project_root(),
             self.tmp_dir_name
@@ -79,6 +120,11 @@ class UpdateManager:
     archive_file_name = 'archive.zip'
 
     def __get_write_path(self):
+        """
+        Returns the path where the archive contents should be written to
+        ====================================================================================================
+        :return:
+        """
         return os.path.join(
             self.__get_save_path(),
             self.archive_file_name
@@ -87,16 +133,32 @@ class UpdateManager:
     unzip_dir_name = 'bin'
 
     def __get_unzip_path(self):
+        """
+        Returns the path where the release archive contents should be unzipped to
+        ====================================================================================================
+        :return:
+        """
         return os.path.join(self.__get_save_path(), self.unzip_dir_name)
 
     grc_parent_dir_name = 'GRC'
 
     def __get_parent_download_path(self):
+        """
+        Returns the path where the release unzipped contents live
+        ====================================================================================================
+        :return:
+        """
         return os.path.join(self.__get_unzip_path(), self.grc_parent_dir_name)
 
     archive_chunk_size = 128
 
     def _write_archive(self, archive):
+        """
+        Writes the binary .zip data to the archive directory for later processing
+        ====================================================================================================
+        :param archive:
+        :return:
+        """
         save_path = self.__get_save_path()
         write_path = self.__get_write_path()
         if not os.path.exists(save_path):
@@ -106,6 +168,11 @@ class UpdateManager:
                 fd.write(chunk)
 
     def _unzip_archive(self):
+        """
+        Unzips the release archive into the working /tmp directory
+        ====================================================================================================
+        :return:
+        """
         write_path = self.__get_write_path()
         unzip_path = self.__get_unzip_path()
         if not os.path.exists(unzip_path):
@@ -124,10 +191,22 @@ class UpdateManager:
 
     excluded_update_directories = [
         'conf',
-        'user'
+        'user',
+        'venv',
+        'tmp'
     ]
 
     def perform_update(self, dir_name: str = None):
+        """
+        Walk through the project and update files that are not protected as:
+            - /user: specific to the implementation or client
+            - /conf: sensitive configuration data, also specific to implementation
+            - system: (venv, tmp, etc.) any directories that are used by the Python / GRC ecosystem
+                and should not be changed
+        ====================================================================================================
+        :param dir_name:
+        :return:
+        """
         parent_dir_name = self.__get_parent_download_path()
         dst_dir_name = self.__get_project_root()
         if dir_name:
@@ -137,15 +216,11 @@ class UpdateManager:
         files = os.listdir(parent_dir_name)
         # for each update-capable file, replace (if exists) or add its equivalent in the live project
         for file in files:
-            if file not in self.excluded_update_files and '.' in file and file != 'venv':
+            if file not in self.excluded_update_files and '.' in file:
                 src_file = os.path.join(parent_dir_name, file)
                 dst_file = os.path.join(dst_dir_name, file)
                 # check for changes
-                with open(src_file, 'r') as src:
-                    scr_file_contents = src.read()
-                with open(dst_file, 'r') as dst:
-                    dst_file_contents = dst.read()
-                if scr_file_contents != dst_file_contents:
+                if self._compare_file_contents(src_file=src_file, dst_file=dst_file):
                     if os.path.isfile(dst_file):
                         os.unlink(dst_file)
                     shutil.copyfile(src_file, dst_file)
@@ -158,8 +233,55 @@ class UpdateManager:
                             _dir_name = file
                         self.perform_update(dir_name=_dir_name)
         # update the 'version' attribute of app.json to the version stored in this class
-
+        self.update_app_version()
+        self.clear_update_zone()
         return
 
+    @staticmethod
+    def _compare_file_contents(src_file: str, dst_file: str) -> bool:
+        """
+        Static helper method to determine if two files are equal by their contents or not
+        ====================================================================================================
+        :param src_file:
+        :param dst_file:
+        :return:
+        """
+        with open(src_file, 'r') as src:
+            scr_file_contents = src.read()
+        with open(dst_file, 'r') as dst:
+            dst_file_contents = dst.read()
+        return scr_file_contents != dst_file_contents
+
+    def update_app_version(self):
+        """
+        Loads the app.json configuration and updates its 'version' attribute in line with the latest release
+        ====================================================================================================
+        :return:
+        """
+        app_config_path = self.__get_app_config_path()
+        with open(app_config_path, 'r') as cnf:
+            app_config = json.load(cnf)
+        app_config['version'] = self.release_version
+        with open(app_config_path, 'w') as cnf:
+            cnf.write(json.dumps(app_config))
+
+    def __get_app_config_path(self):
+        """
+        Returns the path where the app.json configuration file lives
+        ====================================================================================================
+        :return:
+        """
+        return os.path.join(
+            self.__get_project_root(),
+            'conf',
+            'stored',
+            'app.json'
+        )
+
     def clear_update_zone(self):
+        """
+        Recursively deletes files from the /tmp directory where the archive is unzipped to
+        ====================================================================================================
+        :return:
+        """
         shutil.rmtree(self.__get_save_path())
