b98a4aabe7db829f97d26a5b589f235404e8a58e
Jake Schroeder <jschroeder@linkmedia360.com>
2020-07-28 09:17:22 -0400

    main script setup

--- main.py
+++ main.py
@@ -1,18 +1,40 @@
 """
 Main Reporting Workflow Script
 """
-import sys_update
+import os
+import subprocess
 from utils.queue_manager import QueueManager
 
 
 def main() -> None:
     workflow = QueueManager().get()
 
+    root = os.path.abspath(os.path.dirname(__file__))
+    venv_path = os.path.join(root, 'venv', 'Scripts', 'python.exe')
+    script_path = os.path.join(root, 'script.py')
+
     # for each script in workflow.json
     for work_item in workflow:
-
-    # execute with the configured command line args
-
+        name = work_item['name']
+        args = work_item['args']
+        pull = args['pull']
+        ingest = args['ingest']
+        backfilter = args['backfilter']
+        expedited = args['expedited']
+        # execute with the configured command line args
+        call_args = [
+            venv_path,
+            script_path,
+            name,
+            f'--pull={pull}',
+            f'--ingest={ingest}',
+            f'--backfilter={backfilter}',
+            f'--expedited={expedited}'
+        ]
+        print(call_args)
+        subprocess.call(
+            call_args
+        )
     return
 
 
